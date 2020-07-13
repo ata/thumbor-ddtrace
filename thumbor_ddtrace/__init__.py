d7e75cbf7b1de915430a5591974ab8c0c9884904
Ahmad Tanwir <ata@rien.rien.com>
2020-07-13 16:24:59 +0700

    Fix context

--- thumbor_ddtrace/__init__.py
+++ thumbor_ddtrace/__init__.py
@@ -2,7 +2,10 @@ import thumbor.app
 
 class DDTraceThumborServiceApp(thumbor.app.ThumborServiceApp):
     def __init__(self, context):
-        super(DDTraceThumborServiceApp, self).__init__(self.get_handlers(), debug=self.debug, datadog_trace={
+        self.context = context
+        self.debug = getattr(self.context.server, 'debug', False)
+
+        super(thumbor.app.ThumborServiceApp, self).__init__(self.get_handlers(), debug=self.debug, datadog_trace={
             'default_service': 'vidio-thumbor-test',
             'analytics_enabled': True
         })
